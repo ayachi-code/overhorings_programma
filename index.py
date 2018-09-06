1f3e8e2fe817e299a01976854e4207b6a9a986ef
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-06 15:33:50 +0200

    navigatie gemaakt

--- index.py
+++ index.py
@@ -3,4 +3,9 @@ import os
 naam = input("Hallo wat is je naam: ")
 os.system("clear")
 begin = input("Welkom {} type (a) om een woordenlijst te maken of (b) om de woorden te leren ".format(naam))
-
+if (naam == "a"):
+    print("woordenlijst maken")
+elif (naam == "b"):
+    print("woorden leren")
+else:
+    print("hey hey hey de ingevoerde letter is niet geldig")
