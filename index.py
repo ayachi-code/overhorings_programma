06dd4ae2dad6fd4bc3d254c36187ad146677e23e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-06 15:31:46 +0200

    begin input gemaakt waar je een keuzen moest maken

--- index.py
+++ index.py
@@ -2,4 +2,5 @@ import os
 
 naam = input("Hallo wat is je naam: ")
 os.system("clear")
+begin = input("Welkom {} type (a) om een woordenlijst te maken of (b) om de woorden te leren ".format(naam))
 
