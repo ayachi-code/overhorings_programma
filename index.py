57c0702a3fb55231b1aff01eddde871bd853c377
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-11 14:27:10 +0200

    cleared de schern

--- index.py
+++ index.py
@@ -6,12 +6,14 @@ begin = input("Welkom {} type (a) om een woordenlijst te maken of (b) om de woor
 global woordenlijst_naam
 global mape
 global taal
+global nederlands
 
 if (begin == "a"):
     print("woordenlijst maken")
     woordenlijst_naam = input("geef het bestand een naam ")
     mape = os.path.join('woordenlijsten',woordenlijst_naam)
     taal = input("Welke taal ben ja aan het leren(woorden stampen) ")
+    os.system("clear")
 
    # with open(mape,"w+") as f:
     #    bestand_lezen = f.write("hallo ")
